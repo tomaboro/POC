33963a5eca85aa1fcdc6f0c050966486654ee583
tomaboro <motek@student.agh.edu.pl>
2017-12-05 16:15:20 +0100

    lab9

--- lab9/lab9.m
+++ lab9/lab9.m
@@ -123,28 +123,27 @@ image = imread('kwadraty.png');
 imageEdge = edge(image,'canny');
 
 figure(1);
-imshow(imageEdge);
+subplot(1,2,1); imshow(image);
+subplot(1,2,2); imshow(imageEdge);
 
-[H,theta,rho] = hough(image,'RhoResolution',0.1,'ThetaResolution',0.5);
-peaks = houghpeaks(H,8);
+[H,theta,rho] = hough(imageEdge,'RhoResolution',1,'ThetaResolution',1);
+peaks = houghpeaks(H,10);
 
 figure(2);
 imshow(H,[]);
 hold on;
 plot(peaks(:,2),peaks(:,1),'ro');
 
-%%
 figure(3);
 lines = houghlines(imageEdge,theta,rho,peaks,'FillGap',5,'MinLength',7);
-figure, imshow(imageEdge), hold on
 max_len = 0;
 for k = 1:length(lines)
    xy = [lines(k).point1; lines(k).point2];
-   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green'); hold on
 
    % Plot beginnings and ends of lines
-   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow'); hold on
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red'); hold on
 
    % Determine the endpoints of the longest line segment
    len = norm(lines(k).point1 - lines(k).point2);
@@ -153,5 +152,51 @@ for k = 1:length(lines)
       xy_long = xy;
    end
 end
-
 %%
+%czyścimy zmienne
+clearvars;
+clear all;
+clc;
+
+image = imread('lab112.png');
+
+figure(1);
+subplot(1,3,1); imshow(image);
+
+T = 45;
+image(image < T) = 0;
+image(image > T) = 255;
+image(:, 500:640) = 255;
+
+subplot(1,3,2); imshow(image);
+
+image = edge(image,'sobel');
+
+subplot(1,3,3); imshow(image);
+
+[H,theta,rho] = hough(image,'RhoResolution',1,'ThetaResolution',1);
+peaks = houghpeaks(H,10);
+
+figure(2);
+imshow(H,[]);
+hold on;
+plot(peaks(:,2),peaks(:,1),'ro');
+
+figure(3);
+lines = houghlines(image,theta,rho,peaks,'FillGap',5,'MinLength',7);
+max_len = 0;
+for k = 1:length(lines)
+   xy = [lines(k).point1; lines(k).point2];
+   plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green'); hold on
+
+   % Plot beginnings and ends of lines
+   plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow'); hold on
+   plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red'); hold on
+
+   % Determine the endpoints of the longest line segment
+   len = norm(lines(k).point1 - lines(k).point2);
+   if ( len > max_len)
+      max_len = len;
+      xy_long = xy;
+   end
+end
